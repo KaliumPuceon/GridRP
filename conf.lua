1ae935ec40d7e7f0dc58b288d031407d26904bac
kalium <jehansingh.97@gmail.com>
2017-07-21 23:51:17 +0200

    Start adding drawing ability, add basic tiles

--- conf.lua
+++ conf.lua
@@ -1,9 +1,9 @@
 function love.conf(t)
 
-    t.title = "PONG"
+    t.title = "grid"
     t.version = "0.10.2"
-    t.window.width = 800
-    t.window.height = 600
+    t.window.width = 1600
+    t.window.height = 900
 
 end
 
--- main.lua
+++ main.lua
@@ -1,4 +1,65 @@
+-- Gonna make me a D&D grid manager thingamaheck
+-- starting off with a homebrew tiling thing tho
+-- gotta learn how tiles work, and this needs to
+-- be easy enough for me to hack together a room
+-- without having to reach for and use tiled.
+
+-- warning: I have no idea what I am doing
+
+TILE_WIDTH = 128
+
+PORT_X_IN = 16
+PORT_Y_IN = 16
+
+portHeight = 10*TILE_WIDTH
+portWidth = 12*TILE_WIDTH
+
+tile = {
+
+    x = 0,
+    y = 0,
+    terrain = nil,
+    walkable = true,
+    rot = 0,
+    img = nil
+
+}
+
+board = {}
+
+testimg = love.graphics.newImage("assets/tiles/floors/plainGrass.png")
+
+function getTileX(x)
+
+end
+
+function getTileY(y)
+
+end
+
+function drawTiles(area, startx, starty)
+
+    for x=startx,startx+portWidth,1 do
+        for y=starty,starty+portHeight,1 do
+            
+            love.graphics.draw(testimg,(x*TILE_WIDTH)+PORT_X_IN,(y*TILE_WIDTH)+PORT_Y_IN)
+
+        end
+    end
+
+end
+
+function getNearestCorner(X,Y)
+
+end
+
+function getNearestBlock(X,Y)
+
+    
+end
+
 function love.load(arg)
+
 
 end
 
