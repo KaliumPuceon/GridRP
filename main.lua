d797728b0f9cd1f46767d9add044b091021374f4
kalium <jehansingh.97@gmail.com>
2017-07-24 07:32:59 +0200

    Half-assed scaling down. Whole ass that at some point

--- main.lua
+++ main.lua
@@ -10,25 +10,28 @@
 -- don't actually care. 
 
 TILE_WIDTH = 128 --pixel width of tileset
+scale_factor = 1
 
 PORT_X_IN = 8 --border on map window in x
 PORT_Y_IN = 8 --border on map window in y
 
-portHeight = 5 --tile height of window
-portWidth = 5 --tile width of window
+portBaseHeight = 3
+portBaseWidth = 7
+portHeight = portBaseHeight --tile height of window
+portWidth = portBaseWidth--tile width of window
 
 portx = 0 -- which block is in the top left corner x
 porty = 0 -- which block is in the top left corner y
 
 function getTileX(x) --get x placement value for a given tile
 
-    return ((x-portx)*TILE_WIDTH)+PORT_X_IN
+    return ((x-portx)*TILE_WIDTH*scale_factor)+PORT_X_IN
 
 end
 
 function getTileY(y) --get y placement value for a given tile
 
-    return ((y-porty)*TILE_WIDTH)+PORT_Y_IN
+    return ((y-porty)*TILE_WIDTH*scale_factor)+PORT_Y_IN
 
 end
 
@@ -39,7 +42,9 @@ function drawTiles(area,startx, starty) --draw area starting at given coords
             
             if not (area[x][y] == nil) then
 
-                love.graphics.draw(area[x][y].img,getTileX(x),getTileY(y))
+                love.graphics.draw(area[x][y].img,getTileX(x),getTileY(y),
+                area[x][y].rot,scale_factor)
+
             end
 
         end
@@ -50,8 +55,8 @@ end
 function getNearestCorner(x,y,startx,starty) --find nearest corner to point
 
     output = {
-    xout = math.floor(((x-PORT_X_IN)/TILE_WIDTH)+0.5)+startx,
-    yout = math.floor(((y-PORT_Y_IN)/TILE_WIDTH)+0.5)+starty
+    xout = math.floor(((x-PORT_X_IN)/(TILE_WIDTH*scale_factor))+0.5)+startx,
+    yout = math.floor(((y-PORT_Y_IN)/(TILE_WIDTH*scale_factor))+0.5)+starty
     }
     
     return (output)
@@ -60,7 +65,8 @@ end
 
 function getNearestBlock(x,y,startx,starty) --nearest block coord to point
 
-    return getNearestCorner(x-(TILE_WIDTH/2),y-(TILE_WIDTH/2),startx,starty)
+    return getNearestCorner(x-((TILE_WIDTH*scale_factor)/2),y-((TILE_WIDTH*scale_factor)/2)
+    ,startx,starty)
     
 end
 
@@ -139,8 +145,21 @@ function love.update(dt)
 
     blockbug=("block: " .. tostring(blockX).." ".. tostring(blockY))
 
-    if love.mouse.isDown(1) then
+    if love.mouse.isDown(1) and blockX >= portx and blockX <= portx+portWidth
+        and blockY >= porty and blockY <= porty+portHeight then
         board[blockX][blockY].img = selImg
+    end
+
+    if love.keyboard.isDown("r") then
+        love.load()
+    end
+
+    if love.keyboard.isDown("=") then
+        scale_factor = scale_factor + 0.01
+    elseif love.keyboard.isDown("-") then
+        scale_factor = scale_factor - 0.01
+        portHeight = portBaseHeight/scale_factor
+        portWidth = portBaseWidth/scale_factor
     end
 
 
